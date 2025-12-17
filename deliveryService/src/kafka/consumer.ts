5ae899855daafeff4a65b05734cbc198409fb2c4
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-12-17 13:06:44 +0530

    update:- chages

--- deliveryService/src/kafka/consumer.ts
+++ deliveryService/src/kafka/consumer.ts
@@ -1,6 +1,6 @@
 // kafka/consumer.ts
 import { MessagePayload } from "../types/message";
-import { kafka } from "./client"; 
+import { kafka } from "./client";
 
 const consumer = kafka.consumer({ groupId: "delivery-service-group" });
 
@@ -9,41 +9,39 @@ export const startConsumer = async () => {
     await consumer.connect();
     console.log("✅ Kafka Consumer connected");
 
-    await consumer.subscribe({ topic: "chat-service", fromBeginning: true });
-    console.log("📩 Subscribed to topic: chat-messages");
-        await consumer.subscribe({ topic: "message.email", fromBeginning: true });
+    await consumer.subscribe({ topic: "message.email", fromBeginning: true });
     await consumer.subscribe({ topic: "message.sms", fromBeginning: true });
     await consumer.subscribe({ topic: "message.whatsapp", fromBeginning: true });
 
 
     await consumer.run({
-      eachMessage: async ({ topic,  message }) => {
+      eachMessage: async ({ topic, message }) => {
         if (!message.value) return;
-const payload: MessagePayload = JSON.parse(
-        message.value.toString()
-      );
+        const payload: MessagePayload = JSON.parse(
+          message.value.toString()
+        );
         switch (topic) {
-        case "message.email":
-          // handleEmail(payload);
-          console.log("email")
-          break;
+          case "message.email":
+            // handleEmail(payload);
+            console.log("email")
+            break;
 
-        case "message.sms":
-          // handleSMS(payload);
-          console.log("sms")
+          case "message.sms":
+            // handleSMS(payload);
+            console.log("sms")
 
-          break;
+            break;
 
-        case "message.whatsapp":
-          // handleWhatsApp(payload);
-          console.log("whatApps")
+          case "message.whatsapp":
+            // handleWhatsApp(payload);
+            console.log("whatApps")
 
-          break;
+            break;
 
-        default:
-          console.log("Unknown topic", topic);
+          default:
+            console.log("Unknown topic", topic);
+        }
       }
-    }
     });
   } catch (err) {
     console.error("❌ Kafka Consumer error:", err);
